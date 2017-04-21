78874e7dde83ff7e4e2d7f993a99b869d00c98ba
DavidBrueggemann <david.brueggemann@gebit.de>
2017-04-21 13:55:53 +0200

    first commit

---
+++ WatchTuner/WatchTuner WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchTuner WatchKit Extension
+//
+//  Created by David@Work on 04/11/2016.
+//  Copyright © 2016 DavidB. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
